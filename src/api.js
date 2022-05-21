d4a3a99e3197c31c9a0198f2d39691ac1a8e9f77
Jean Viegas <jeanmelg@gmail.com>
2022-05-21 10:30:59 -0300

    feat: get user by id endpoint

--- src/api.js
+++ src/api.js
@@ -64,6 +64,17 @@ app.get('/user', authenticate, async (req, res) => {
   return res.status(200).json(users);
 });
 
+app.get('/user/:id', authenticate, async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findByPk(id, {
+    attributes: { exclude: ['password'] },
+   });
+
+  if (!user) return res.status(404).json({ message: 'User does not exist' });
+
+  return res.status(200).json(user);
+});
+
 // É importante exportar a constante `app`,
 // para que possa ser utilizada pelo arquivo `src/server.js`
 module.exports = app;
