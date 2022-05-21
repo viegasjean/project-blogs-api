823e46d2d431d0a189b64582c9a7cbcd1d168193
Jean Viegas <jeanmelg@gmail.com>
2022-05-20 21:44:47 -0300

    fix: fix associations on PostCategory  model

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,28 @@
+'use strict';
+ const PostCategory = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER
+  }, {
+    timestamps: false
+  });
+
+  PostCategory.associate = (models) => {
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'id',
+      otherKey: 'id',
+    });
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPost',
+      through: PostCategory,
+      foreignKey: 'id',
+      otherKey: 'id',
+    });
+  };
+
+  return PostCategory;
+};
+
+module.exports = PostCategory;
